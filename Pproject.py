90d3ae83d8848937cf1f3a3f80f12916f0bc6729
michael-mk12 <khourymichael27@gmail.com>
2024-07-17 09:41:19 +0300

    wghat a roller coser of event yesterday was so long story short progame was working all the sudden stops where it doesn't add into the dictinory idk why had to some going about gave up and saw what are the solutions my face when i could have just used global ...  :..-(  but whatver basiclly what i  did is also used the cities as global and to add them i will in my final functions use it as a way to see if one driver can go from point a to point b by .3 or maybe .4 betwen there longtidite aka x and altidude aka y and check both if the starting location can go to the other locations and thats why i put and other dictionary in the end anw i will cry now

--- Pproject.py
+++ Pproject.py
@@ -1,4 +1,23 @@
-
+RyanGosling={
+            0:{
+            "ID":1,
+            "name":"Max verstappen",
+            "location":"beirut","destination":"jounieh",},
+            1:{
+            "ID":2,
+            "name":"Lewis Hamilton",
+            "location":"sidon","destination":"tyre",},
+            2:{
+            "ID":3,
+            "name":"Daniel Ricciardo",
+            "location":"tripoli","destination":"hermel",
+              },
+            }
+cities1=[]
+cities2=[]
+for i in range(len(RyanGosling)):
+      cities1.append(RyanGosling[i]["location"])
+      cities2.append(RyanGosling[i]["destination"])
 def main_menu():
   FaileSafe=True  
   while FaileSafe:
@@ -7,7 +26,7 @@ def main_menu():
       if user_info=="1":
         driver_menu()
       elif user_info=="2":
-        print(2)
+        city_menu()
       elif user_info =="3":
          print("Goodbye")
          return False
@@ -17,6 +36,29 @@ def main_menu():
           print("2.to go to the citie's menu.")
           print("3.to exit ")
 
+def city_menu():
+    FaileSafe=True  
+    print("Welcome to the city's menu enter the following: ")
+    print("1.To go to view all the  cities.")
+    print("2.to add a city.")
+    print("3.to check which driver can get to a city.")
+    print("4.to go back to main menu.")
+    while FaileSafe:
+      print("------City menu------")
+      user_info = input("Enter your choice: ")
+      if user_info=="1":
+       show_city()
+      elif user_info=="2":
+        add_city()
+      elif user_info =="3":
+         print()
+      elif user_info =="4":
+          return False
+      else:
+          print("Invalid input, please try again.")
+          print("1.To go to view all the  cities.")
+          print("2.to add a city.")
+          print("3.to go back to the main menu.")
 def driver_menu():
   FaileSafe=True  
   print("Welcome to the drivers menu enter the following: ")
@@ -38,53 +80,76 @@ def driver_menu():
           print("2.to add a driver.")
           print("3.to go back to the main menu.")
 def view_driver():
-   RyanGosling={
-                0:{
-                "ID":1,
-                "name":"Max verstappen",
-                "location":"beirut","destination":"jounieh",},
-                1:{
-                "ID":2,
-                "name":"Lewis Hamilton",
-                "location":"sidon","destination":"tyre",},
-                2:{
-                "ID":3,
-                "name":"Daniel Ricciardo",
-                "location":"tripoli","destination":"hermel",
-                  },
-                }
-   for i in range(len(RyanGosling)):
-      print(str(RyanGosling[i])[1:-1])
-   return RyanGosling
+  global RyanGosling
+  for i in range(len(RyanGosling)):
+      print(RyanGosling[i])
+  return RyanGosling
 def add_driver():
+    global RyanGosling,cities1,cities2
     faileSafe = True
-    RyanGosling = view_driver()
     while faileSafe:
             user_1 = input("Do you wish to add a driver y/n: ")
             if user_1.lower() == "y":
-                  new_driver_info_2 = input("Enter the driver name: ")
-                  new_driver_info_3 = input("Enter the driver location: ")
-                  new_driver = {
-                        "ID": len(RyanGosling)+1,
-                        "name": new_driver_info_2,
-                        "location": new_driver_info_3
-                    }
-                  RyanGosling[len(RyanGosling)] = new_driver
-                  print(RyanGosling)
+             
+              new_driver_info_2 = input("Enter the driver name: ")
+              new_driver_info_3 = input("Enter the driver location: ")
+              new_driver_info_4 = input("Enter the driver destination: ")
+              new_driver= {
+                    "ID": len(RyanGosling)+1,
+                    "name": new_driver_info_2,
+                    "location": new_driver_info_3,
+                    "destination":new_driver_info_4,
+              }
+              RyanGosling[len(RyanGosling)] = new_driver
+              print(RyanGosling)
             elif user_1.lower() == "n":
                 faileSafe = False
                 print("Returning to the main menu")
-                return False
             else:
                 print("Invalid input, please try again.")
-                print("y/n")      
-def show_cities():
-  x=[33.8,33,5,34.4]
-  y=[35.4,35.3,35.8]
-  x1=[33.9,33.2,34.3]
-  y2=[35.6,35.2,36.3]
+                print("y/n")
+    cities1.append(new_driver_info_3) 
+    cities2.append(new_driver_info_4)
+def show_city():
+   global cities1, cities2
+   print("All cities are:  ",cities1, cities2)
+   return cities1, cities2
+
+def add_city():
+   global cities2, cities1
+   failSafe = True
+   while failSafe:
+    user1=input("Do you wish to add a city y/n")
+    if user1.lower() == "y":
+        new_city=input("Enter the city name: ")
+        if new_city in cities1 or cities2:
+            print("City already exists")
+            add_city()
+        cities1.append(new_city)
+        print(q)
+    elif user1.lower() == "n":
+        failSafe = False
+        print("Returning to the main menu")
+        main_menu()
+        return False 
+    else:
+        print("Invalid input, please try again.")
+        print("y/n")
+      
+def check_diliver():
+  start={1:{"x":33.8,"y":35.4},
+         2:{"x":33.5,"y":35.3},
+         3:{"x":33.4,"y":35.8},
+         }
+  end={1:{"x":33.9,"y":35.6},
+       2:{"x":33.2,"y":35.2},
+       3:{"x":34.3,"y":36.2},
+       }
   max_x=34.6
   max_y=36
+  min_x=33.1
+  min_y=35.1
+
 # failsafe = True
 # while failsafe:
 #     print("Hello please enter the following: ")
@@ -92,9 +157,9 @@ def show_cities():
 #     print("2.to go to the citie's menu.")
 #     print("3.to exit ")
 #     failsafe = main_menu()
-z=1
-w= view_driver()
-for i in range(len(w)):
-  if w[i]["destination"]:
-     if w[i]["destination"]["x"] == z:
-      print("done ")
+  # for i in range(1,4):
+  #   if start[i]["x"]<=max_x and start[i]["x"]>=min_x and start[i]["y"]<=max_y and start[i]["y"]>=min_y and end[i]["x"]<=max_x and end[i]["x"]>=min_x and end[i]["y"]<=max_y and end[i]["y"]>=min_y:
+  #     print(f"The driver with ID {i} can get to the city from {start[i]['x']},{start[i]['y']} to {end[i]['x']},{end[i]['y']}")
+  #   else:
+  #     print(f"The driver with ID {i} can not get to the city from {start[i]['x']},{start[i]['y']} to {end[i]['x']},{end[i]['y']}")
+main_menu()
